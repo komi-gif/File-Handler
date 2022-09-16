0d2525f7e429a54848ab75192ba63391a4a85486
komi-gif <53171173+komi-gif@users.noreply.github.com>
2022-09-16 15:02:31 +0800

    Update print_files.py

--- handling pdf/print_files.py
+++ handling pdf/print_files.py
@@ -4,7 +4,7 @@ import win32api
 import win32print
 import os
 
-path = 'D:\\SynologyDrive\\FY01a【归档】物业尾款一期\\【纸质归档】材料整理\\项目组签字文件\\4 PDF_for_Print\\'
+path = 'add your folder path'
 filenames = os.listdir(path)
 for name in filenames:
     open(path+name,"r")
